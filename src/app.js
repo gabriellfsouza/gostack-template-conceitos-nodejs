2c4278259626888881cfd1f08490f9261622b227
Gabriel Lima F. de Souza <gabriellfsouza@gmail.com>
2020-04-10 20:48:31 -0300

    feat: challange finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,50 @@ app.use(cors());
 
 const repositories = [];
 
+function verifyIfIdExist(request,response,next){
+  const {id} = request.params;
+  const rep = repositories.find(r=>r.id===id);
+  if (!rep) return response.status(400).send('Bad request');
+  next();
+}
+
+app.use('/repositories/:id/*',verifyIfIdExist);
+app.use('/repositories/:id',verifyIfIdExist);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const id = uuid();
+  const repository = {id,title, url, techs, likes: 0};
+  repositories.push(repository);
+  return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+  const {id} = request.params;
+  const rep = repositories.find(r=>r.id===id);
+  if(title) rep.title = title;
+  if(url) rep.url = url;
+  if(techs) rep.techs = techs;
+  return response.status(200).json(rep);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const index = repositories.findIndex(r=>r.id===id);
+  repositories.splice(index,1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const rep = repositories.find(r=>r.id===id);
+  rep.likes += 1;
+  return response.status(200).json(rep);
 });
 
 module.exports = app;
